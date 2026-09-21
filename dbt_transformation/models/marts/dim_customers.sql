864241cdda01704de389ba9e29a18ac69861a8b2
Janujan Gathieswaran <Janujan.g@hotmail.com>
2026-09-20 21:27:39 -0400

    Add dbt models and configurations for customers and orders; update .gitignore to include key.json

---
+++ dbt_transformation/models/marts/dim_customers.sql
@@ -0,0 +1,22 @@
+with 
+    customers as (
+        select * from {{ ref('stg_customers') }}
+    ),
+    orders as (
+        select * from {{ ref('stg_orders') }}
+    )
+    customer_orders as (
+        select 
+            c.customer_id,
+            c.email,
+            c.country,
+            c.city,
+            min(o.order_approved_at) as first_order_date,
+            max(o.order_approved_at) as most_recent_order_date,
+            count(o.order_id) as total_orders,
+        from orders o
+        join customers c using (customer_id)
+        group by c.customer_id, c.email, c.country, c.city
+    )
+select *
+from customer_orders
